df342e0c54816198f55e7ea34aed939a61022026
vbeakovic <valter.beakovic@gmail.com>
2015-11-14 17:56:02 +0100

    first version of run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,13 @@
+## run_analysis.R
+## Step 1 ##
+## Read in activities
+proba <- read.table("./uci_har_dataset/train/X_train.txt")
+# 3. A code book that describes the variables the data and any transformations or work that you performed
+# to clean up the data called CodeBook.md
+# 4. A README.md file describes how the scripts work
+
+
+
+
+proba <- read.table("./uci_har_dataset/train/X_train.txt")
+
